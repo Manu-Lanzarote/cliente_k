5d7847e0ce4ec66bef804977446e784dd8a221e5
Manu-Lanzarote <madrid29@hotmail.com>
2021-01-12 18:07:09 +0000

    imagenes productos

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
-import MenuNav from "./components/landing/MenuNav";
-import Main from "./components/landing/Main";
-import LaMarque from "./components/landing/LaMarque";
-import LaCollection from "./components/landing/LaCollection";
-import Histoire from "./components/landing/Histoire";
-import ModalApp from "./components/landing/ModalApp";
-import Contact from "./components/landing/Contact";
+// import { BrowserRouter, Route } from "react-router-dom";
+// import MenuNav from "./components/landing/MenuNav";
+// import Main from "./components/landing/Main";
+// import LaMarque from "./components/landing/LaMarque";
+// import LaCollection from "./components/landing/LaCollection";
+// import Histoire from "./components/landing/Histoire";
+// import ModalApp from "./components/landing/ModalApp";
+// import Contact from "./components/landing/Contact";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="contenedor_principal">
+//         <div className="contenedor_menu_vertical">
+//           <MenuNav />
+//         </div>
+//         <div className="contenedor_contenidos">
+//           <Route exact path="/">
+//             <Main />
+//           </Route>
+//           <Route exact path="/LaMarque">
+//             <LaMarque />
+//           </Route>
+//           <Route exact path="/LaCollection">
+//             <LaCollection />
+//           </Route>
+//           <Route exact path="/Histoire">
+//             <Histoire />
+//           </Route>
+//           <Route exact path="/ModalApp">
+//             <ModalApp />
+//           </Route>
+//           <Route exact path="/Contact">
+//             <Contact />
+//           </Route>
+//         </div>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+import Single_product_img from "./components/shop/Single_product_img";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="contenedor_principal">
-        <div className="contenedor_menu_vertical">
-          <MenuNav />
-        </div>
-        <div className="contenedor_contenidos">
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/LaMarque">
-            <LaMarque />
-          </Route>
-          <Route exact path="/LaCollection">
-            <LaCollection />
-          </Route>
-          <Route exact path="/Histoire">
-            <Histoire />
-          </Route>
-          <Route exact path="/ModalApp">
-            <ModalApp />
-          </Route>
-          <Route exact path="/Contact">
-            <Contact />
-          </Route>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+  return <Single_product_img />;
 }
 
 export default App;
---
+++ src/components/shop/Single_product_img.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const Single_product_img = () => {
+  return "imagenes de producto";
+};
+
+export default Single_product_img;
